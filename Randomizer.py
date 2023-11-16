0a3a64c79a7be377d88333ebc783e03abcd55557
EvinExiNiTy <javadfeyux@gmail.com>
2023-11-16 21:20:42 +0330

    initial Commit.
    the first commit of this Repo, Randomizer.py is Now included.

---
+++ Randomizer.py
@@ -0,0 +1,88 @@
+import random
+from time import sleep
+
+try:
+    def main():
+        entries_list = []
+        entries = input("How many Elements Are there?\n>> ")
+        try:
+            entries = int(entries)
+        except:
+            print("YOU HAD ONE FUCKING JOB, CHOOSE A NUMBER; A NUMBER!")
+            main()
+        for entry_num in range(entries):
+            entries_list.append(input(f"What is Entry Number {entry_num+1}'s Alias?\n>> "))
+        continue_q = input(f"Is this Right? (Y/n/q):\n\t{entries_list}\n>> ")
+        if continue_q.lower() in ("y", ""):
+            app(entries_list)
+        
+        elif continue_q.lower() == "n":
+            main()
+        
+        elif continue_q.lower() == "q":
+            print("Fuck off.")
+            sleep(1)
+            quit()
+        
+    def app(entries):
+        def timerator():
+            times = random.randint(random.randint(0,255), random.randint(256, 512))
+            times *= random.randint(random.randint(8,16), random.randint(17,64))
+            return times
+        
+        print(f"Ok, Calculating an OutPut out of {len(entries)} Entries...")
+        if timerator() % 2 == 0:
+            print("Trying Harder...")
+            timerator()
+        else:
+            pass
+        score = {}
+        for obj in entries:
+            score.update({obj: 0})
+        
+        silence = input("for the Last question, Should i Disable Silent Mode? It's \"True\" By default. (True, False)\n>> ")
+        while silence.lower() not in ("true", "false", ""):
+            print("You messed up, Try again.")
+            silence = input("Should i Disable Silent Mode? It's \"True\" By default. (True, False)\n>> ")
+
+        for time in range(timerator()):
+            score[random.choice(entries)] += 1
+            if silence.lower() == "false":
+                print(score)
+
+        sleep(1)
+        print("~!----------------------------------------!~")
+        print("Done Calculating, Getting Results...")
+        sleep(0.2)
+        print("Okay, Finished! Printing Results...")
+        sleep(0.2)
+        print(f"So, This is The score for Each of the Elements you Gave Me:\n{score}")
+        winner = max(score, key=score.get)
+        print("...and The Winner is NO ONE BUT!!!!:\n")
+        sleep(2)
+        print(winner + " !!!!!!!!!")
+        sleep(2)
+        print(f"GOOD JOB {winner}, YOU JUST FUCKING WON!!!!!")
+        print(f"(after --> {timerator()} <-- Attempts by the way.")
+        reset_q = input("Should i Restart the app or quit? (q/r)\n>> ")
+        if reset_q.lower() == "q":
+            print("fuck off Bitch.")
+            sleep(2)
+            exit()
+        elif reset_q.lower() == "r":
+            print("Restarting...")
+            print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+            main()
+        elif reset_q.lower() not in ("q", "r"):
+            print("You messed up, but ima Close up anyway; (Stop messing up you Bozo)")
+            sleep(3)
+            exit()
+        #return winner # Uncomment if you ever Wanted to add Saving-To-File Feature to this Miserable program.
+    main()
+
+except KeyboardInterrupt:
+    print("You wasted my Time. FUCK OFF.")
+    sleep(1)
+finally:
+    print("App Exited/Terminated.")
+    sleep(1)
